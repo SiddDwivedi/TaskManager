ec2d53360fa29169dabcebdbbd0b502096fe9129
SiddDwivedi <siddwivedi88@gmail.com>
2026-05-17 17:12:26 +0530

    fix: simplify frontend start command

--- frontend/src/pages/ProjectDetail.jsx
+++ frontend/src/pages/ProjectDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import api from '../api/client';
 import Layout from '../components/Layout';
